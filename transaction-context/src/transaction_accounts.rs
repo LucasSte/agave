7147338506e2c9f9ec81d768a4420d3ece2c9c12
Lucas <lucas.tnagel@gmail.com>
2025-09-26 16:37:31 -0300

    New transaction layout

--- transaction-context/src/transaction_accounts.rs
+++ transaction-context/src/transaction_accounts.rs
@@ -1,5 +1,7 @@
+use std::sync::Arc;
 #[cfg(feature = "dev-context-only-utils")]
 use qualifier_attr::qualifiers;
+use solana_account::{ReadableAccount, WritableAccount};
 use {
     crate::{IndexOfAccount, MAX_ACCOUNT_DATA_GROWTH_PER_TRANSACTION, MAX_ACCOUNT_DATA_LEN},
     solana_account::AccountSharedData,
@@ -11,6 +13,116 @@ use {
     },
 };
 
+#[repr(C)]
+#[derive(Debug)]
+struct SVMAccount {
+    key: Pubkey,
+    owner: Pubkey,
+    lamports: u64,
+    // The payload is going to be filled with the guest virtual address of the account payload
+    // vector.
+    // payload: VmSlice,
+}
+
+#[derive(Debug)]
+struct SVMAccountDeprecated {
+    rent_epoch: u64,
+    executable: bool,
+}
+
+struct PrivateAccountFields {
+    deprecated_fields: SVMAccountDeprecated,
+    payload: Arc<Vec<u8>>,
+}
+
+struct TransactionAccountView<'a> {
+    svm_account: &'a SVMAccount,
+    deprecated_fields: &'a SVMAccountDeprecated,
+    payload: &'a [u8],
+}
+
+impl ReadableAccount for TransactionAccountView<'_> {
+    fn lamports(&self) -> u64 {
+        self.svm_account.lamports
+    }
+
+    fn data(&self) -> &[u8] {
+        self.payload
+    }
+
+    fn owner(&self) -> &Pubkey {
+        &self.svm_account.owner
+    }
+
+    fn executable(&self) -> bool {
+        self.deprecated_fields.executable
+    }
+
+    fn rent_epoch(&self) -> u64 {
+        self.deprecated_fields.rent_epoch
+    }
+}
+
+#[derive(Debug)]
+struct TransactionAccountMutView<'a> {
+    svm_account: &'a mut SVMAccount,
+    deprecated_fields: &'a mut SVMAccountDeprecated,
+    payload: &'a mut Arc<Vec<u8>>,
+}
+
+impl ReadableAccount for TransactionAccountMutView<'_> {
+    fn lamports(&self) -> u64 {
+        self.svm_account.lamports
+    }
+
+    fn data(&self) -> &[u8] {
+        self.payload.as_slice()
+    }
+
+    fn owner(&self) -> &Pubkey {
+        &self.svm_account.owner
+    }
+
+    fn executable(&self) -> bool {
+        self.deprecated_fields.executable
+    }
+
+    fn rent_epoch(&self) -> u64 {
+        self.deprecated_fields.rent_epoch
+    }
+}
+
+impl WritableAccount for TransactionAccountMutView<'_> {
+    fn set_lamports(&mut self, lamports: u64) {
+        self.svm_account.lamports = lamports;
+    }
+
+    fn data_as_mut_slice(&mut self) -> &mut [u8] {
+        Arc::make_mut(self.payload).as_mut_slice()
+    }
+
+    fn set_owner(&mut self, owner: Pubkey) {
+        self.svm_account.owner = owner;
+    }
+
+    fn copy_into_owner_from_slice(&mut self, source: &[u8]) {
+        self.svm_account.owner.as_mut().copy_from_slice(source);
+    }
+
+    fn set_executable(&mut self, executable: bool) {
+        self.deprecated_fields.executable = executable;
+    }
+
+    fn set_rent_epoch(&mut self, epoch: u64) {
+        self.deprecated_fields.rent_epoch = epoch;
+    }
+
+    fn create(_lamports: u64, _data: Vec<u8>, _owner: Pubkey, _executable: bool, _rent_epoch: u64) -> Self {
+        panic!("It is not possible to create a TransactionMutView");
+    }
+}
+
+
 /// An account key and the matching account
 pub type TransactionAccount = (Pubkey, AccountSharedData);
 pub(crate) type OwnedTransactionAccounts = (
@@ -21,7 +133,8 @@ pub(crate) type OwnedTransactionAccounts = (
 
 #[derive(Debug)]
 pub struct TransactionAccounts {
-    accounts: UnsafeCell<Box<[TransactionAccount]>>,
+    shared_account_metadata: UnsafeCell<Box<[SVMAccount]>>,
+    private_account_fields: UnsafeCell<Box<[PrivateAccountFields]>>,
     borrow_counters: Box<[BorrowCounter]>,
     touched_flags: Box<[Cell<bool>]>,
     resize_delta: Cell<i64>,
@@ -33,9 +146,26 @@ impl TransactionAccounts {
     pub(crate) fn new(accounts: Vec<TransactionAccount>) -> TransactionAccounts {
         let touched_flags = vec![Cell::new(false); accounts.len()].into_boxed_slice();
         let borrow_counters = vec![BorrowCounter::default(); accounts.len()].into_boxed_slice();
-        let accounts = UnsafeCell::new(accounts.into_boxed_slice());
+        let (shared_accounts, private_fields) = accounts.into_iter().map(|item|
+            (
+                SVMAccount {
+                    key: item.0,
+                    owner: *item.1.owner(),
+                    lamports: item.1.lamports(),
+                },
+                PrivateAccountFields {
+                    deprecated_fields: SVMAccountDeprecated {
+                        rent_epoch: item.1.rent_epoch(),
+                        executable: item.1.executable(),
+                    },
+                    payload: item.1.data_clone(),
+                }
+            )
+        ).collect::<(Vec<SVMAccount>, Vec<PrivateAccountFields>)>();
+
         TransactionAccounts {
-            accounts,
+            shared_account_metadata: UnsafeCell::new(shared_accounts.into_boxed_slice()),
+            private_account_fields: UnsafeCell::new(private_fields.into_boxed_slice()),
             borrow_counters,
             touched_flags,
             resize_delta: Cell::new(0),
@@ -45,7 +175,7 @@ impl TransactionAccounts {
 
     pub(crate) fn len(&self) -> usize {
         // SAFETY: The borrow is local to this function and is only reading length.
-        unsafe { (*self.accounts.get()).len() }
+        unsafe { (*self.shared_account_metadata.get()).len() }
     }
 
     #[cfg(not(target_os = "solana"))]
@@ -102,7 +232,14 @@ impl TransactionAccounts {
         // SAFETY: The borrow counter guarantees this is the only mutable borrow of this account.
         // The unwrap is safe because accounts.len() == borrow_counters.len(), so the missing
         // account error should have been returned above.
-        let account = unsafe { &mut (*self.accounts.get()).get_mut(index as usize).unwrap().1 };
+        let svm_account = unsafe { (*self.shared_account_metadata.get()).get_mut(index as usize).unwrap() };
+        let private_fields = unsafe { (*self.private_account_fields.get()).get_mut(index as usize).unwrap() };
+
+        let account = TransactionAccountMutView {
+            svm_account,
+            deprecated_fields: &mut private_fields.deprecated_fields,
+            payload: &mut private_fields.payload
+        };
 
         Ok(AccountRefMut {
             account,
@@ -120,7 +257,14 @@ impl TransactionAccounts {
         // SAFETY: The borrow counter guarantees there are no mutable borrow of this account.
         // The unwrap is safe because accounts.len() == borrow_counters.len(), so the missing
         // account error should have been returned above.
-        let account = unsafe { &(*self.accounts.get()).get(index as usize).unwrap().1 };
+        let svm_account = unsafe { (*self.shared_account_metadata.get()).get(index as usize).unwrap() };
+        let private_fields = unsafe { (*self.private_account_fields.get()).get(index as usize).unwrap() };
+
+        let account = TransactionAccountView {
+            svm_account,
+            deprecated_fields: &private_fields.deprecated_fields,
+            payload: &private_fields.payload
+        };
 
         Ok(AccountRef {
             account,
@@ -142,8 +286,17 @@ impl TransactionAccounts {
         self.lamports_delta.get()
     }
 
+    pub(crate) fn into_account_shared_data(self) -> Vec<AccountSharedData> {
+        self.shared_account_metadata.get_mut().into_iter().zip(
+            self.private_account_fields.get_mut().into_iter()
+        ).map(
+            |(shared_fields, private_fields)|
+                AccountSharedData::shared
+        )
+    }
+
     pub(crate) fn take(self) -> OwnedTransactionAccounts {
-        (self.accounts, self.touched_flags, self.resize_delta)
+        (self.shared_account_metadata, self.touched_flags, self.resize_delta)
     }
 
     pub fn resize_delta(&self) -> i64 {
@@ -152,12 +305,12 @@ impl TransactionAccounts {
 
     pub(crate) fn account_key(&self, index: IndexOfAccount) -> Option<&Pubkey> {
         // SAFETY: We never modify an account key, so returning a reference to it is safe.
-        unsafe { (*self.accounts.get()).get(index as usize).map(|acc| &acc.0) }
+        unsafe { (*self.shared_account_metadata.get()).get(index as usize).map(|acc| &acc.key) }
     }
 
     pub(crate) fn account_keys_iter(&self) -> impl Iterator<Item = &Pubkey> {
         // SAFETY: We never modify account keys, so returning an immutable reference to them is safe.
-        unsafe { (*self.accounts.get()).iter().map(|item| &item.0) }
+        unsafe { (*self.shared_account_metadata.get()).iter().map(|item| &item.key) }
     }
 }
 
@@ -214,7 +367,7 @@ impl BorrowCounter {
 }
 
 pub struct AccountRef<'a> {
-    account: &'a AccountSharedData,
+    account: TransactionAccountView<'a>,
     borrow_counter: &'a BorrowCounter,
 }
 
@@ -224,16 +377,16 @@ impl Drop for AccountRef<'_> {
     }
 }
 
-impl Deref for AccountRef<'_> {
-    type Target = AccountSharedData;
+impl<'a> Deref for AccountRef<'a> {
+    type Target = TransactionAccountView<'a>;
     fn deref(&self) -> &Self::Target {
-        self.account
+        &self.account
     }
 }
 
 #[derive(Debug)]
 pub struct AccountRefMut<'a> {
-    account: &'a mut AccountSharedData,
+    account: TransactionAccountMutView<'a>,
     borrow_counter: &'a BorrowCounter,
 }
 
@@ -243,16 +396,16 @@ impl Drop for AccountRefMut<'_> {
     }
 }
 
-impl Deref for AccountRefMut<'_> {
-    type Target = AccountSharedData;
+impl<'a> Deref for AccountRefMut<'a> {
+    type Target = TransactionAccountMutView<'a>;
     fn deref(&self) -> &Self::Target {
-        self.account
+        &self.account
     }
 }
 
-impl DerefMut for AccountRefMut<'_> {
+impl<'a> DerefMut for AccountRefMut<'a> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        self.account
+        &mut self.account
     }
 }
 
