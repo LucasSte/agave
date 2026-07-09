7e0595e93a1fe1f1edef32d1e475d28c2ca60ea0
Simonas Kazlauskas <github@kazlauskas.me>
2026-07-09 23:21:39 +0300

    abiv2: sysvars (#12)
    * sysvar regions take 1
    
    * cleanup and handle missing vars in the cache
    
    cache can have no entries if we're testing *or* if we have added a new
    sysvar and it hasn't been yet enabled. In either case producing a
    0-sized region for these variables is adequate solution.
    
    For metadata this is going to be slightly more involved as we will want
    to not include the still-disabled sysvars in the list of accounts. But
    for enabled ones probably should still include an entry even if the
    sysvar is not available in the cache?
    
    * restore the memory_context test
    
    * populate sysvars backwards
    
    * fixup sbf tests
    
    This check feels like it could be written instead as a allowlist of
    indices rather than 3 distinct loops...
    
    * rebase
    
    * adjust the memory layout to introduce the padding
    
    * basic sysvar tests

--- syscalls/src/lib.rs
+++ syscalls/src/lib.rs
@@ -8564,7 +8564,7 @@ mod tests {
         let config = Config::default();
         prepare_mockup!(invoke_context, program_id, bpf_loader_upgradeable::id());
         invoke_context.memory_contexts.set_abi_v2().unwrap();
-        let mut regions = create_abiv2_regions(invoke_context.transaction_context);
+        let mut regions = create_abiv2_regions(&mut invoke_context);
         for region in &mut regions {
             unsafe {
                 // SAFETY: never actually reading or writing host buffers.
@@ -8589,7 +8589,7 @@ mod tests {
         let config = Config::default();
         prepare_mockup!(invoke_context, program_id, bpf_loader_upgradeable::id());
         invoke_context.memory_contexts.set_abi_v2().unwrap();
-        let mut regions = create_abiv2_regions(invoke_context.transaction_context);
+        let mut regions = create_abiv2_regions(&mut invoke_context);
         for region in &mut regions {
             unsafe {
                 // SAFETY: never actually reading or writing host buffers
@@ -8615,7 +8615,7 @@ mod tests {
         let config = Config::default();
         prepare_mockup!(invoke_context, program_id, bpf_loader_upgradeable::id());
         invoke_context.memory_contexts.set_abi_v2().unwrap();
-        let mut regions = create_abiv2_regions(invoke_context.transaction_context);
+        let mut regions = create_abiv2_regions(&mut invoke_context);
         for region in &mut regions {
             unsafe {
                 // SAFETY: never actually reading or writing host buffers.
@@ -8676,7 +8676,7 @@ mod tests {
         with_mock_invoke_context!(invoke_context, transaction_context, 0, transaction_accounts);
 
         let new_owner = Pubkey::new_unique();
-        let mut regions = create_abiv2_regions(invoke_context.transaction_context);
+        let mut regions = create_abiv2_regions(&mut invoke_context);
         *regions.get_mut(1).unwrap() =
             MemoryRegion::new(&raw const new_owner.as_array()[..], 1u64 << 32);
         let account_region_index =
@@ -8852,7 +8852,7 @@ mod tests {
             )
             .unwrap();
 
-        let regions = create_abiv2_regions(invoke_context.transaction_context);
+        let regions = create_abiv2_regions(&mut invoke_context);
         let mapping = unsafe {
             MemoryMapping::new(regions.clone(), &Config::default(), SBPFVersion::V4).unwrap()
         };
