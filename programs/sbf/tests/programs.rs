4f39301bf3228a2102cd1444c72aa04342fe5665
LucasSte <lucas.tnagel@gmail.com>
2026-09-03 15:31:02 -0300

    Test account copy

--- programs/sbf/tests/programs.rs
+++ programs/sbf/tests/programs.rs
@@ -165,8 +165,8 @@ fn process_transaction_and_record_inner(
         executed_units,
         ..
     } = commit_result.unwrap();
-    let inner_instructions = inner_instructions.expect("cpi recording should be enabled");
-    let log_messages = log_messages.expect("log recording should be enabled");
+    let inner_instructions = inner_instructions.unwrap_or_default(); //.expect("cpi recording should be enabled");
+    let log_messages = log_messages.unwrap_or_default(); //.expect("log recording should be enabled");
     (status, inner_instructions, log_messages, executed_units)
 }
 
@@ -5492,4 +5492,103 @@ fn test_program_sbf_rust_direct_account_pointers(num_accounts: usize, input_data
         (num_accounts * 2).to_le_bytes().to_vec(),
         effects.return_data
     );
+}
+
+#[test]
+fn authorize_nonce() {
+    agave_logger::setup();
+
+    let GenesisConfigInfo {
+        genesis_config,
+        mint_keypair,
+        ..
+    } = create_genesis_config(100_123_456_789);
+
+    let rent = Rent::default();
+    let mut bank = Bank::new_for_tests(&genesis_config);
+    let (bank, bank_forks) = bank.wrap_with_bank_forks_for_tests();
+    let mut bank_client = BankClient::new_shared(bank.clone());
+    let bank = bank_client
+        .advance_slot(1, &bank_forks, SlotLeader::default())
+        .unwrap();
+
+    let mut authority = Keypair::new();
+    let nonce = Keypair::new();
+
+    let authority_data = AccountSharedData::new(6467979786515, 0, &system_program::id());
+    bank.store_account(&authority.pubkey(), &authority_data);
+
+    let create_ix = solana_system_interface::instruction::create_account(
+        &authority.pubkey(), &nonce.pubkey(),
+        rent.minimum_balance(200 as usize),
+        200, &system_program::id(),
+    );
+
+    let create_tx = Transaction::new_signed_with_payer(
+        &[create_ix], Some(&authority.pubkey()), &[authority.insecure_clone(), nonce.insecure_clone()],
+        bank.last_blockhash()
+    );
+
+    let (result, _cpis, logs, _) = process_transaction_and_record_inner(&bank, create_tx);
+    std::println!("Tx1");
+    std::println!("res: {:?}", result);
+    std::println!("logs: {:?}", logs);
+
+    let initialize_ix = Instruction::new_with_bincode(
+        system_program::id(),
+        &solana_system_interface::instruction::SystemInstruction::InitializeNonceAccount(authority.pubkey()),
+        vec![
+            AccountMeta::new(nonce.pubkey(), false),
+            AccountMeta::new_readonly(sysvar::recent_blockhashes::id(), false),
+            AccountMeta::new_readonly(sysvar::rent::id(), false),
+        ],
+    );
+    let initialize_tx = Transaction::new_signed_with_payer(
+        &[initialize_ix], Some(&authority.pubkey()), &[authority.insecure_clone()],
+        bank.last_blockhash()
+    );
+
+    let (result, _cpis, logs, _) = process_transaction_and_record_inner(&bank, initialize_tx);
+    std::println!("Tx2");
+    std::println!("res: {:?}", result);
+    std::println!("logs: {:?}", logs);
+
+    let mut authorize_ixs = vec![
+        ComputeBudgetInstruction::set_compute_unit_limit(100_000),
+        ComputeBudgetInstruction::set_compute_unit_price(10_000_000),
+        Instruction::new_with_bytes(Pubkey::new_unique(), &[], vec![]),
+    ];
+
+
+    let now = std::time::Instant::now();
+    for _ in 0..50 {
+        let new_authority = Keypair::new();
+        *authorize_ixs.get_mut(2).unwrap() =
+            solana_system_interface::instruction::authorize_nonce_account(
+                &nonce.pubkey(), &authority.pubkey(), &new_authority.pubkey(),
+            );
+        let slow_tx = Transaction::new_signed_with_payer(
+            &authorize_ixs,
+            Some(&authority.pubkey()),
+            &[authority.insecure_clone()],
+            bank.last_blockhash(),
+        );
+        let (result, _cpis, logs, _) = process_transaction_and_record_inner(&bank, slow_tx);
+        authority = new_authority.insecure_clone();
+    }
+    let elapsed = now.elapsed();
+    std::println!("Total time: {}", elapsed.as_micros());
+
+    //
+    // let slow_tx = Transaction::new_signed_with_payer(
+    //     &authorize_ixs,
+    //     Some(&authority.pubkey()),
+    //     &authorize_keypairs,
+    //     bank.last_blockhash(),
+    // );
+    //
+    // let (result, _cpis, logs, _) = process_transaction_and_record_inner(&bank, slow_tx);
+    // std::println!("Tx3");
+    // std::println!("res: {:?}", result);
+    // std::println!("logs: {:?}", logs);
 }
